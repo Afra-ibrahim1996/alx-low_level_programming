30099a1debb35cc9a96ba3fd35de207723e8c80b
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-13 15:43:42 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 1 (not Success)
 */
+
 int main(void)
-{
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n". 59);
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
 	return (1);
 }
