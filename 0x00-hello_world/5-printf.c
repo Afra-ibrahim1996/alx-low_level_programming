a94d0ce6205b24f7df0a5dfb458d0791940c1ae5
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-13 13:19:48 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
+ * Description : A C program that print with pritf function
+ *
  * Return: Always  0 (success)
  */
+
 int main (void)
 {
 	printf"with proper grammar, but the outcome is a piece of art,\n";
 		retune (0);
 }
-
