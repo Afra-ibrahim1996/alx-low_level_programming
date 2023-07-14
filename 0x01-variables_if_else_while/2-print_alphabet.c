194f91094df03beaf9ab6a101919928766351636
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-14 10:45:10 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stidio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char ch - "a";
+	while (char <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
