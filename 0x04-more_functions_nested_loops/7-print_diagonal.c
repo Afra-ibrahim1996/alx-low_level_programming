8c558fae23e18e46bec904b9ecd95ba180fad572
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-21 04:43:19 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,19 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n <= 0)
-		_putchar('\n');
-	else
-	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 1; j <= n; j++)
-			{
-				if (j == i)
+			if (n < 1)
+				for (j = 0; j < n; j++)
+				{
+					_putchar(' ');
+				}
 				_putchar('\\');
-				else if (j < i)
-				_putchar (' ');
-			}
+				_putchar('\n');
+		}
+		if (n <= 0)
+		{
 			_putchar('\n');
 		}
-	}
 }
 
