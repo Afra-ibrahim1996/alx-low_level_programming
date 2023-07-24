22a61f73278ce80faa31b88833ebb1d4dc1c82e9
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-24 15:58:55 -0700

    aa

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
-/** _strcpy - copies the string pointedto be src
+/** *_strcpy - copies the string pointed to by src
+ * @dest: char type string
+ * @src: char type string
  *
- * Return: pointer to dest
+ * Return: pointer to  dest
 */
 
 char *_strcpy(char *dest, char *src)
@@ -12,7 +14,7 @@ char *_strcpy(char *dest, char *src)
 	do {
 		i++;
 		dest[i] = src[i];
-	} while (src[i] != '\0');
+	} while (src[i] != '0');
 
 	return (dest);
 }
