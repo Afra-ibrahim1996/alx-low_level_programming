52ac1de31bfc211c9bcac9b00a8398ea1db34db7
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-27 10:42:17 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,23 +16,24 @@ char *cap_string(char *str)
 	{
 		while (str[index] >= 97 && str[index] <= 122)
 			index++;
+
 		if (str[index - 1] == ' ' ||
-		str[indes - 1] == '\t' ||
-		str[index - 1] == '\n' ||
-		str[index - 1] == ',' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == '.' ||
-		str[index - 1] == '!' ||
-		str[index - 1] == '?' ||
-		str[index - 1] == '"' ||
-		str[index - 1] == ')' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == '{' ||
-		str[index - 1] == '}' ||
-		index == 0)
+			str[indes - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+
 			str[index] -= 32;
 
-		index++;
 	}
 	return (str);
 }
