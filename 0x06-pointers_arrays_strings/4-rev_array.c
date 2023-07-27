bfaa7976d6a6294e824e946c93dbc71d70aca368
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-27 07:21:28 -0700

    a

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,12 @@
 
 void reverse_arry(int *a, int n)
 {
-	int i, t;
+	int i, j, t;
 
-	for (i = 0; i < n--; i++)
+	for (i = 0, j = (n - 1); i < j; i++, j--)
 	{
 		t = a[i];
-		a[i] = a[n];
-		a[n] = t;
+		a[i] = a[j];
+		a[j] = t;
 	}
 }
