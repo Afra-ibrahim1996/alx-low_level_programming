9a1edd13e56de7f0a570460311dba1a63c2d3831
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-27 10:29:04 -0700

    a

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * *cap_string - function that capitalize all words of string
+ *
+ * @str : string to return
+ *
+ * Return: @str
+*/
+
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 97 && str[index] <= 122))
+			index++;
+		if
+		{
+			(str[index - 1] == ' ' ||
+			str[indes - 1] == '\t' ||
+			str[index - 1] == '\n' ||
+			str[index - 1] == ',' ||
+			str[index - 1] == ';' ||
+			str[index - 1] == '.' ||
+			str[index - 1] == '!' ||
+			str[index - 1] == '?' ||
+			str[index - 1] == '"' ||
+			str[index - 1] == ')' ||
+			str[index - 1] == '(' ||
+			str[index - 1] == '{' ||
+			str[index - 1] == '}' ||
+			index == 0)
+				str[index] -= 32;
+
+		index++;
+	}
+	return (str);
+}
