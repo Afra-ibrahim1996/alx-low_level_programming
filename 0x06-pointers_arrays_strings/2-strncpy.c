1a4b026280f733aa7a730762960f99969033c0c7
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-27 05:55:35 -0700

    aa

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copy strings
+ *
+ * @dest: pointer to destination input
+ * @src: pointer to source input 
+ * @n: most number of byte in @src
+ *
+ * Return: @dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
