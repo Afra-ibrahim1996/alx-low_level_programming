fc58856eacc0e61eafac28f401733fad4d8e92d1
afra-ibrahim1996 <afra23466@gmail.com>
2023-07-27 10:59:36 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 97 && str[index] <= 122))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 
 		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
