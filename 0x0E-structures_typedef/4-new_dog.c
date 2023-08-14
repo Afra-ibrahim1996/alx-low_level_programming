0fd988f4a6a7f52a04a2cc5a8745cd3f0db7c5bd
afra-ibrahim1996 <afra23466@gmail.com>
2023-08-14 12:38:34 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -51,8 +51,9 @@ char *_strcopy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-		if (!name || age < 0 || owner)
-			return (NULL);
+
+	if (!name || age < 0 || owner)
+		return (NULL);
 
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (dog == NULL)
