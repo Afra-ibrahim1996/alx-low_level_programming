f2e73e09d33d1b0cd8520d4bd978dc7c68fecf4d
afra-ibrahim1996 <afra23466@gmail.com>
2023-08-16 14:45:50 -0700

    m

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: number of args
+ * @argv: argument vactor
+ * Return: 0 (Success)
+*/
+
+int main(int argc, char **argv)
+{
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Erorr\n"), exit(1);
+	b = atio(argv[1]);
+	if (b < 0)
+		printf("Erorr\n"), exit(2);
+
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
+}
