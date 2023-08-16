43dc965f6b187d14a8b6d35d343791e974fbb37a
afra-ibrahim1996 <afra23466@gmail.com>
2023-08-16 15:10:03 -0700

    abubaker

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,16 +11,33 @@
 
 int main(int argc, char **argv)
 {
-	char *p = (char *)main;
-	int b;
+	int byte, index;
+	int (*address)(int, char **) = main;
+	unsigned char opcode;
 
 	if (argc != 2)
-		printf("Erorr\n"), exit(1);
-	b = atoi(argv[1]);
-	if (b < 0)
-		printf("Erorr\n"), exit(2);
+	{
+		printf("Erorr\n");
+		exit(1);
+	}
+	byte = atoi(argv[1]);
+	if (byte < 0)
+	{
+		printf("Erorr\n");
+		exit(2);
+	}
 
-	while (b--)
-		printf("%02hhx%s", *p++, b ? " " : "\n");
+	for (index = 0, index < byte; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == byte - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+	printf("\n");
 	return (0);
 }
