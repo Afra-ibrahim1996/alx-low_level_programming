cde4ad7d06ae029e632d8e3d69aac076bec1340c
afra-ibrahim1996 <afra23466@gmail.com>
2023-08-17 07:18:35 -0700

    n

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -46,7 +46,8 @@ void format_string(char *separator, va_list ap)
 	char *str = va_arg(ap, char *);
 
 	switch ((int)(!str))
-	case 1:
+
+		case 1:
 		str = "(nil)";
 
 	printf("%s%s", separator, var_arg(ap, str));
