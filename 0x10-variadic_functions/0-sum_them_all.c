9397f7e4365519d6250418503213012da0c80b22
afra-ibrahim1996 <afra23466@gmail.com>
2023-08-17 06:02:27 -0700

    m

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,4 @@
-#include "vaiadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - function that returns the sum of all its parameters
