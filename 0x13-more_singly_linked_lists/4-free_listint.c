83c9cc6ca5476297383ef470fa47db2dd155c59e
afra-ibrahim1996 <afra23466@gmail.com>
2023-08-28 05:18:58 -0700

    a

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint - that frees a listint_t list
+ * @head: pointer to first node
+ *
+ * Return: void
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+}
