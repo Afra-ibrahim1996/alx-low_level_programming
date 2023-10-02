6d28ca43fa29834d25249f17747c144d43723919
afra-ibrahim1996 <afra23466@gmail.com>
2023-10-02 15:13:21 -0700

    b

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,12 +27,12 @@ int _strlen(char *s)
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes= 0, len = _strlen(text_content);
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd = -1)
+	if (fd == -1)
 		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
